beb72985e08a3111bdbfa66434ca3070a0294ed1
Vlad Dracula <kaikopecky@ucsb.edu>
2019-02-22 10:26:10 -0800

    initializing my code for r carpentry

---
+++ notes.R
@@ -0,0 +1,11 @@
+# Vector = group of numbers
+
+# "+" indicates an incomplete operation
+
+# "Functions" are the tools we use in R, while "arguments" are the specifications for the function to use
+
+# Typing "?" before a function can tell not only the arguments that can be used by a function, but the package (in {}) that needs to be installed/loaded to use the function
+
+#the "vignettes" function will be bring up useful resources for some packages
+
+# useful online resources: rbloggers, stackoverflow, rdocumentation
